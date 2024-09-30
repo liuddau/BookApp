7c3cb8087b754171a837b6e0a84c39b24aae86e8
Liudas Dauzvardis <liuddau@gmail.com>
2024-09-30 22:04:16 +0300

    creating data file and inserting data to DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Books (ID, TITLE , YEAR_OF_RELEASE , AUTHOR , RATING) VALUES (1, 'Debesu atlasas', 2014, 'David Mitchell', 4.5);
+INSERT INTO Books (ID, TITLE , YEAR_OF_RELEASE , AUTHOR , RATING) VALUES (2, 'Triumfo arka', 2016, 'Erich Maria Remarque', 4.9);
+INSERT INTO Books (ID, TITLE , YEAR_OF_RELEASE , AUTHOR , RATING) VALUES (3, 'Anyta', 2019, 'Sally Hepworth', 3.7);
+INSERT INTO Books (ID, TITLE , YEAR_OF_RELEASE , AUTHOR , RATING) VALUES (4, 'Vienuoliktas isakymas', 2021, 'Abraham Verghese', 3.5);
+INSERT INTO Books (ID, TITLE , YEAR_OF_RELEASE , AUTHOR , RATING) VALUES (5, 'Violeta', 2022, 'Isabel Allende', 2.3);
