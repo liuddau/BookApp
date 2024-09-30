a587cc1973569b8d93b026faefbf19a1d3eb789c
Liudas Dauzvardis <liuddau@gmail.com>
2024-09-30 22:53:56 +0300

    creating filtering by title, year, author and rating

--- src/main/java/com/example/BookApp/controller/BookController.java
+++ src/main/java/com/example/BookApp/controller/BookController.java
@@ -36,11 +36,67 @@ public class BookController {
 
     @GetMapping("/getBookById/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable Long id) {
-        Optional<Book> bookObj = bookRepository.findById(id);
-        if (bookObj.isPresent()) {
+        try {
+            Optional<Book> bookObj = bookRepository.findById(id);
+            if (bookObj.isPresent()) {
+                return new ResponseEntity<>(bookObj.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getBookByTitle/{title}")
+    public ResponseEntity<List<Book>> getBookByTitle(@PathVariable String title) {
+        try {
+            Optional<List<Book>> bookObj = bookRepository.findByTitleContainingIgnoreCase(title);
+            if (bookObj.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
             return new ResponseEntity<>(bookObj.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getBookYearOfRelease/{year}")
+    public ResponseEntity<List<Book>> getBookYearOfRelease(@PathVariable Integer year) {
+        try {
+            Optional<List<Book>> bookObj = bookRepository.findByYearOfRelease(year);
+            if (bookObj.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(bookObj.get(), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getBookByAuthor/{author}")
+    public ResponseEntity<List<Book>> getBookByAuthor(@PathVariable String author) {
+        try {
+            Optional<List<Book>> bookObj = bookRepository.findByAuthorContainingIgnoreCase(author);
+            if (bookObj.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(bookObj.get(), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getBookByRating/{rating}")
+    public ResponseEntity<List<Book>> getBookByRating(@PathVariable Double rating) {
+        try {
+            Optional<List<Book>> bookObj = bookRepository.findByRatingGreaterThanEqual(rating);
+            if (bookObj.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(bookObj.get(), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/example/BookApp/repo/BookRepository.java
+++ src/main/java/com/example/BookApp/repo/BookRepository.java
@@ -1,10 +1,22 @@
 package com.example.BookApp.repo;
 
 import com.example.BookApp.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<List<Book>> findByTitleContainingIgnoreCase(String title);
+
+    Optional<List<Book>> findByAuthorContainingIgnoreCase(String author);
+
+    Optional<List<Book>> findByYearOfRelease(Integer yearOfRelease);
+
+    Optional<List<Book>> findByRatingGreaterThanEqual(Double rating);
 
 }
